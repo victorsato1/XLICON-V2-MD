edb24d35fa6cfd7668ca1cf0f73d8e6bcfff3957
Victor Sato <127440022+victorsato1@users.noreply.github.com>
2024-01-07 16:03:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['233533763772', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['918360234087', 'WHITE', true], 
+  ['2349131778689', 'VICTOR SATO', true],
+  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true],
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
@@ -54,7 +53,7 @@ global.APIKeys = { // APIKey Here
 global.botname = 'XLICON-V2'
 global.premium = 'true'
 global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.author = '@V-SATO' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
